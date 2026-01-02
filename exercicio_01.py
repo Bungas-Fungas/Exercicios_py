555e2a177d7896bdad1416ccc4bbdd96fee98d26
Bungas-Fungas <antoniolosisalvador@gmail.com>
2026-01-02 12:59:22 -0300

    Update exercicio_01.py

--- exercicio_01.py
+++ exercicio_01.py
@@ -1,4 +1,25 @@
+##---------------Exercicio 01------------------------
+
 nome = input("Qual seu nome:")
 peso = input("Qual seu peso:")
 idade = input("Qual sua idade:")
 print(nome,idade,peso)
+
+##---------------Exercicio 02------------------------
+
+nome = input("Qual seu nome:") 
+print("Seja bem vindo",nome+"!")
+
+##---------------Exercicio 03------------------------
+
+dia = int(input("Em que dia você nasceu:"))
+mes = int(input("Em que mes você nasceu:"))
+ano = int(input("Em que ano você nasceu:"))
+print("Você nasceu no dia",dia,"de",mes,"de",ano,"!")
+
+
+##---------------Exercicio 04------------------------
+
+num1 = int(input("primeiro numero:"))
+num2 = int(input("segundo numero:"))
+print(num1+num2)
