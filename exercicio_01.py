91fa92bb0cf8b685a115c55f9d637bda9c40475a
Bungas-Fungas <antoniolosisalvador@gmail.com>
2026-01-05 14:57:33 -0300

    a

--- exercicio_01.py
+++ exercicio_01.py
@@ -26,3 +26,6 @@ print("Você nasceu no dia",dia,"de",mes,"de",ano,"!")
 num1 = int(input("primeiro numero:"))
 num2 = int(input("segundo numero:"))
 print(num1+num2)
+
+##---------------Exercicio 05------------------------
+
---
+++ exercicio_02.py
@@ -0,0 +1,20 @@
+##---------------Ex001------------
+
+##print mostra coisas na tela.
+print("olá, mundo!")
+
+msg = "Ola, mundo!"
+print(msg)
+
+##---------------Ex002------------
+
+nome= str(input("Qual é o seu nome:"))
+
+## {} é substituido pela saida formatada
+print("Seja muito bem vindo {}!".format(nome))
+
+##outro jeito d fazer
+print("Seja muito bem vindo",nome+"!")
+
+
+##---------------Ex003------------
