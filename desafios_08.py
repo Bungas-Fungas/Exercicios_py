380b889c0a02b826bdc1ea9c46d277af5df894b3
Bungas-Fungas <antoniolosisalvador@gmail.com>
2026-01-09 14:37:01 -0300

    Update desafios_08.py

--- desafios_08.py
+++ desafios_08.py
@@ -42,7 +42,8 @@ n1= str(input('Nome do primeiro aluno: '))
 n2= str(input('Nome do segundo aluno: '))
 n3= str(input('Nome do terceiro aluno: '))
 n4= str(input('Nome do quarto aluno: '))
-s= random.randint(n1,n2,n3,n4)
+lista = [n1,n2,n3,n4]
+s= random.choice(lista)
 print('O sorteado foi {}'.format(s))
 
 ## 5: ler o nome de 4 alunos e sortar uma ordem de apresentação
