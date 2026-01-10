85f498a442333123fd9dead2bb90bc2c85e9098f
Bungas-Fungas <antoniolosisalvador@gmail.com>
2026-01-10 17:26:39 -0300

    Update desafios_09.py

---
+++ desafios_09.py
@@ -0,0 +1,13 @@
+##Desafios aula 09 curso em video python mundo 1
+
+## 1: ler o nome completo de uma pessoa e mostrar o nome em maiusco,minusculos,todas as letras sem os espacos e quantas letras tem o primeiro nome
+
+## 2: ler um numero e mostrar cada digito separado
+
+## 3: ler o nome de uma cidade e dizer se começa ou nao com o nome 'santo'
+
+## 4: ler o nome de alguem e dizer se a pessoa tem 'silva' no nome
+
+## 5: ler uma frase e mostrar quantas vezes aparece a letra 'a', em que posicao ela aparece na primeira vez e qual posicao ela aparece pela ultima vez
+
+## 6: ler um nome e mostrar o primeiro e o ultimo nome
