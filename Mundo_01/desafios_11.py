e8d4c8a4cbb38daa73bcb4361f16256d6031e700
Bungas-Fungas <antoniolosisalvador@gmail.com>
2026-01-23 13:29:56 -0300

    organização d pasta

---
+++ Mundo_01/desafios_11.py
@@ -0,0 +1,5 @@
+print('\033[7;33;44mOlá Mundo!\033[m')
+nome = 'Bungas'
+print('Olá, {}{}{} você tem um belo nome.'.format('\033[4;34m',nome,'\033[m'))
+
+## colocar cores em 10 desafios 
--- desafios_11.py
+++
@@ -1,3 +0,0 @@
-print('\033[7;33;44mOlá Mundo!\033[m')
-nome = 'Bungas'
-print('Olá, {}{}{} você tem um belo nome.'.format('\033[4;34m',nome,'\033[m'))
