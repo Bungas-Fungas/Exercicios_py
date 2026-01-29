0e6b91c44b47598e8ef2b847cf31c69d2a8ad55d
Bungas-Fungas <antoniolosisalvador@gmail.com>
2026-01-29 20:02:29 -0300

    teste adiciionado

--- Mundo_02/Desafios_01.py
+++ Mundo_02/Desafios_01.py
@@ -120,7 +120,24 @@ else:
 
     ## desafio 11: fazer o computador jogar jokenpô com o usuario
 
-    from random import choice
-    jokenpo = ['Pedra','Papel','Tesoura']
-    escolha_pc = choice(jokenpo)
-    
+from random import choice
+jokenpo = [1,2,3]
+escolha_pc = choice(jokenpo)
+escolha_usuario = int(input("""Escolha o que irá jogar:
+                            [1] Pedra
+                            [2] Papel
+                            [3] Tesoura\n"""))
+if(escolha_pc == 1 and escolha_usuario == 3):
+    print('O computador escolheu pedra, você perdeu.')
+elif(escolha_pc == 2 and escolha_usuario == 1):
+    print('O computador escolheu papel, você perdeu.')
+elif(escolha_pc == 3 and escolha_usuario == 2):
+    print('O computador escolheu Tesoura, você perdeu.')
+elif(escolha_pc == 1 and escolha_usuario == 2):
+    print('O computador escolheu pedra, você ganhou.')
+elif(escolha_pc == 2 and escolha_usuario == 3):
+    print('O computador escolheu papel, você ganhou.')
+elif(escolha_pc == 3 and escolha_usuario == 1):
+    print('O computador escolheu tesoura, você ganhou.')
+else:
+    print('O computador escolheu o mesmo que você, empatou.')
